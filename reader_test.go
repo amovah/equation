7a28441a99492139e4533f9609dc57e76b02a7e0
Ali Movahedi <ali_movahedi@aol.com>
2019-08-05 21:36:34 +0430

    feat: add one more test case for splitter

--- reader_test.go
+++ reader_test.go
@@ -6,35 +6,36 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSplitter1(t *testing.T) {
-	all := splitter("23+log(10,5)")
+func TestSplitter(t *testing.T) {
+	case1 := splitter("23+log(10,5)")
+	assert.Equal(t, "23", case1[0])
+	assert.Equal(t, "+", case1[1])
+	assert.Equal(t, "log", case1[2])
+	assert.Equal(t, "(", case1[3])
+	assert.Equal(t, "10", case1[4])
+	assert.Equal(t, ",", case1[5])
+	assert.Equal(t, "5", case1[6])
+	assert.Equal(t, ")", case1[7])
 
-	assert.Equal(t, "23", all[0])
-	assert.Equal(t, "+", all[1])
-	assert.Equal(t, "log", all[2])
-	assert.Equal(t, "(", all[3])
-	assert.Equal(t, "10", all[4])
-	assert.Equal(t, ",", all[5])
-	assert.Equal(t, "5", all[6])
-	assert.Equal(t, ")", all[7])
-}
+	case2 := splitter("+(-4)")
+	assert.Equal(t, "+", case2[0])
+	assert.Equal(t, "(", case2[1])
+	assert.Equal(t, "-", case2[2])
+	assert.Equal(t, "4", case2[3])
+	assert.Equal(t, ")", case2[4])
 
-func TestSplitter2(t *testing.T) {
-	all := splitter("+(-4)")
+	case3 := splitter("2+e")
+	assert.Equal(t, "2", case3[0])
+	assert.Equal(t, "+", case3[1])
+	assert.Equal(t, "e", case3[2])
 
-	assert.Equal(t, "+", all[0])
-	assert.Equal(t, "(", all[1])
-	assert.Equal(t, "-", all[2])
-	assert.Equal(t, "4", all[3])
-	assert.Equal(t, ")", all[4])
-}
-
-func TestSplitter3(t *testing.T) {
-	all := splitter("2+e")
-
-	assert.Equal(t, "2", all[0])
-	assert.Equal(t, "+", all[1])
-	assert.Equal(t, "e", all[2])
+	case4 := splitter("((+43))")
+	assert.Equal(t, "(", case4[0])
+	assert.Equal(t, "(", case4[1])
+	assert.Equal(t, "+", case4[2])
+	assert.Equal(t, "43", case4[3])
+	assert.Equal(t, ")", case4[4])
+	assert.Equal(t, ")", case4[5])
 }
 
 func TestNextElement(t *testing.T) {
