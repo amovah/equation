5c0adf9372108a0ddf249220288c14c536f01132
Ali Movahedi <ali_movahedi@aol.com>
2019-08-07 11:03:42 +0430

    style: rename parameter name

--- calculator.go
+++ calculator.go
@@ -14,39 +14,39 @@ func splitIntoTwo(str []string, index int) (string, string) {
 	return gather(str, 0, index), gather(str, index+1, len(str))
 }
 
-func commaHandler(str string, userOperators map[string]operators.Operator) []float64 {
+func commaHandler(str string, operators map[string]operators.Operator) []float64 {
 	extracted := extractOperators(createReader(str))
 	result := make([]float64, 0)
 	lastIndex := 0
 	for _, v := range extracted {
 		if v.symbol == "," {
-			result = append(result, calculate(gather(splitter(str), lastIndex, v.index), userOperators))
+			result = append(result, calculate(gather(splitter(str), lastIndex, v.index), operators))
 			lastIndex = v.index + 1
 		}
 	}
 
 	if len(result) == 0 {
-		return []float64{calculate(str, userOperators)}
+		return []float64{calculate(str, operators)}
 	}
 
 	return result
 }
 
-func min(arr []sign, userOperators map[string]operators.Operator) sign {
+func min(arr []sign, operators map[string]operators.Operator) sign {
 	min := arr[0]
-	minPriority := userOperators[min.symbol].Priority
+	minPriority := operators[min.symbol].Priority
 
 	for _, v := range arr {
-		if userOperators[v.symbol].Priority < minPriority {
+		if operators[v.symbol].Priority < minPriority {
 			min = v
-			minPriority = userOperators[v.symbol].Priority
+			minPriority = operators[v.symbol].Priority
 		}
 	}
 
 	return min
 }
 
-func calculate(str string, userOperators map[string]operators.Operator) float64 {
+func calculate(str string, operators map[string]operators.Operator) float64 {
 	splitted := splitter(str)
 	extracted := extractOperators(createReader(str))
 	if len(extracted) == 0 {
@@ -70,20 +70,20 @@ func calculate(str string, userOperators map[string]operators.Operator) float64 
 
 	var low sign
 	if len(withoutInner) > 0 {
-		low = min(withoutInner, userOperators)
+		low = min(withoutInner, operators)
 	} else {
-		low = min(hasInner, userOperators)
+		low = min(hasInner, operators)
 	}
 
 	if low.innerExpression == "" {
 		left, right := splitIntoTwo(splitted, low.index)
-		return userOperators[low.symbol].Operation(
-			calculate(left, userOperators),
-			calculate(right, userOperators),
+		return operators[low.symbol].Operation(
+			calculate(left, operators),
+			calculate(right, operators),
 		)
 	} else {
-		return userOperators[low.symbol].Operation(
-			commaHandler(low.innerExpression, userOperators)...,
+		return operators[low.symbol].Operation(
+			commaHandler(low.innerExpression, operators)...,
 		)
 	}
 }
